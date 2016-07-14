9d5e0e3488fe4a30105644ca380d66819f986eb2
dev26 <dev26@vaibhav.pathak@panaceatek.com>
2016-07-14 19:27:14 +0530

    Deleted Bk Folder

--- app/PiplModulesBK/EmailTemplate/Views/email-change.blade.php
+++
@@ -1,4 +0,0 @@
-<html><body>Hello {{$FIRST_NAME}} {{$LAST_NAME}}, Your email has been changed successfully. Please verify your email by clicking on the link provided belowground
-        <br><br><a href="{{$VERIFICATION_LINK}}">{{$VERIFICATION_LINK}}</a><br></br>
- Thanks,
- Code Library Team
--- app/PiplModulesBK/EmailTemplate/Views/request-reset-password.blade.php
+++
@@ -1,6 +0,0 @@
-<p>Hello {{$USER_NAME}}, Please see below link to reset your password.<br />
-<br />
-<a href="{{$RESET_LINK}}">{{$RESET_LINK}}</a><br />
-<br />
-Thanks,<br />
-PIPL Lib Team</p>
